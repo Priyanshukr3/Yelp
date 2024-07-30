5a579afc56a62d6eb1478e25289115a03ca26c15
Priyanshu <krpriyanshu3@gmail.com>
2024-07-31 02:39:03 +0530

    update1

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const userRoutes = require("./routes/users");
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoDBStore = require("connect-mongo")(session);
-const dbUrl = 'mongodb://127.0.0.1:27017/YelpCamp';
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/YelpCamp';
  
 mongoose.connect(dbUrl)
  .then(()=>{
